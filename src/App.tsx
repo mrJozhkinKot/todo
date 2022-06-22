e5138a489229f04968691239c4f953a5fc0820b2
mrJozhkinKot <j.shchuklina@icloud.com>
2022-06-22 23:34:28 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import About from './components/pages/About';
+import Main from './components/pages/Main';
 
 const App = () => {
   return (
     <div className="App">
-    Todo
+    <Routes>
+      <Route path='/' element={<Layout />}>
+         <Route index element={<Main />}/>
+       <Route path='/about' element={<About />} />
+      </Route>
+    </Routes>
     </div>
   );
 }
